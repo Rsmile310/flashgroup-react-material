29c84c0ad2e197c18cba891a3496c531827eed35
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-07-18 23:46:21 +0900

    update

--- src/components/home/oztgcoin/OztgCard.js
+++ src/components/home/oztgcoin/OztgCard.js
@@ -4,17 +4,18 @@ export default function OztgCard() {
   return (
     <div className="card">
       <div className="top-data">
-        <img src="/assets/images/oztg_hexagon.svg" alt="oztg coin" />
+        <Box
+          component="img"
+          src="/assets/images/oztg_hexagon.svg"
+          sx={{ background: "transparent !important" }}
+        />
         <h3 className="crypto-name">OZTG</h3>
         <h4 className="crypto-price">
           $1.05<span className="positive">+0.20%</span>
         </h4>
       </div>
       <div className="chart">
-        <Box
-          component="img"
-          src="/assets/images/chart.svg"
-        />
+        <Box component="img" src="/assets/images/chart.svg" />
       </div>
     </div>
   );
